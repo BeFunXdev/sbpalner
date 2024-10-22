e814d23fc9daf93ad8c202921d67bb059d23173a
BeFunXdev <105725119+BeFunXdev@users.noreply.github.com>
2024-10-22 21:49:40 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix();
+  app.setGlobalPrefix('');
   app.use(cookieParser());
   app.enableCors({
     origin: ['http://localhost:3000', 'http://213.171.30.54:3000'],
