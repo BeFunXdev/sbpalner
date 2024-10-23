a6595b1099cee563f5c57c498ae2a3c2ba182265
Vlas <gj123a1@mail.ru>
2024-10-23 16:06:01 +0300

    fix

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -56,7 +56,7 @@ export class AuthService {
             expires: expiresIn,
             secure: true,
             //lax if production
-            sameSite: 'strict'
+            sameSite: 'lax'
         })
     }
 
@@ -67,7 +67,7 @@ export class AuthService {
             expires: new Date(0),
             secure: true,
             //lax if production
-            sameSite: 'strict'
+            sameSite: 'lax'
         })
     }
 
